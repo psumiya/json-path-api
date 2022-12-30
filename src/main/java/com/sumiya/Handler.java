07bea69fefb3ed9d6e480c0f9b448ad5047a483c
psumiya <sumiya.p@gmail.com>
2022-12-29 18:17:05 -0800

    Add lambda handler

---
+++ src/main/java/com/sumiya/Handler.java
@@ -0,0 +1,22 @@
+package com.sumiya;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    private static final String SUCCESSFUL = """
+            {
+              "hello": "world"
+            }
+            """;
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        return response.withBody(SUCCESSFUL).withStatusCode(200);
+    }
+
+}
