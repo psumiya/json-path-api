57f50908b9988151c4ff936eecc982d9b4773eb3
psumiya <sumiya.p@gmail.com>
2023-01-01 22:50:53 -0800

    Refactor unit tests to use ParameterizedTest

--- src/test/java/com/sumiya/JsonPathEvaluatorTest.java
+++ src/test/java/com/sumiya/JsonPathEvaluatorTest.java
@@ -1,13 +1,31 @@
 package com.sumiya;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
 public class JsonPathEvaluatorTest {
 
     private static final String DEFAULT_ROOT = "src/test/resources/";
+
+    static Stream<Arguments> testFilesProvider() {
+        return Stream.of(
+                arguments(DEFAULT_ROOT + "withAllOptions.json", """
+                ["bar1",null]"""),
+                arguments(DEFAULT_ROOT + "withSomeOptions.json", """
+                ["bar1",null]"""),
+                arguments(DEFAULT_ROOT + "withoutOptions.json", """
+                {"hello":"world"}"""),
+                arguments(DEFAULT_ROOT + "badExpression.json", """
+                {"message":"Path must not end with a '.' or '..'","errorContext":{"context":"Unexpected error occurred while evaluating jsonPath."}}""")
+        );
+    }
 
     private JsonPathEvaluator fixture;
 
@@ -16,36 +34,12 @@ public class JsonPathEvaluatorTest {
         fixture = new JsonPathEvaluator();
     }
 
-    @Test
-    public void happyPathWithAllOptions() {
-        String json = FileReader.getFileContentAsString(DEFAULT_ROOT + "withAllOptions.json");
+    @ParameterizedTest
+    @MethodSource("testFilesProvider")
+    public void testSamples(String path, String expectedResult) {
+        String json = FileReader.getFileContentAsString(path);
         String evaluated = fixture.evaluate(json);
-        assertEquals("""
-                ["bar1",null]""", evaluated);
-    }
-
-    @Test
-    public void happyPathWithSomeOptions() {
-        String json = FileReader.getFileContentAsString(DEFAULT_ROOT + "withSomeOptions.json");
-        String evaluated = fixture.evaluate(json);
-        assertEquals("""
-                ["bar1",null]""", evaluated);
-    }
-
-    @Test
-    public void happyPathWithoutOptions() {
-        String json = FileReader.getFileContentAsString(DEFAULT_ROOT + "withoutOptions.json");
-        String evaluated = fixture.evaluate(json);
-        assertEquals("""
-                {"hello":"world"}""", evaluated);
-    }
-
-    @Test
-    public void withBadPathExpression() {
-        String json = FileReader.getFileContentAsString(DEFAULT_ROOT + "badExpression.json");
-        String evaluated = fixture.evaluate(json);
-        assertEquals("""
-                {"message":"Path must not end with a '.' or '..'","errorContext":{"context":"Unexpected error occurred while evaluating jsonPath."}}""", evaluated);
+        assertEquals(expectedResult, evaluated);
     }
 
 }
